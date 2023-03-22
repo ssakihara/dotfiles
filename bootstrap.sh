67671f7464db1f63c6f5a478f134c099c6f57a38
Sakihara Shinnosuke <39264557+ssakihara@users.noreply.github.com>
2023-03-23 02:14:47 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,26 +5,26 @@ cd "$(dirname "${BASH_SOURCE:-$0}")";
 git pull origin main;
 
 function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude "macos.sh" \
-		--exclude "brew.sh" \
-		--exclude "install.sh" \
-		--exclude "bootstrap.sh" \
-		--exclude "Brewfile" \
-		--exclude "README.md" \
-		-avh --no-perms . ~;
-	# source ~/.zshrc;
+  rsync --exclude ".git/" \
+        --exclude ".DS_Store" \
+        --exclude "macos.sh" \
+        --exclude "brew.sh" \
+        --exclude "install.sh" \
+        --exclude "bootstrap.sh" \
+        --exclude "Brewfile" \
+        --exclude "README.md" \
+        -avh --no-perms . ~;
+  # source ~/.zshrc;
   touch .credentials;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+  doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt;
+  fi;
 fi;
 unset doIt;
